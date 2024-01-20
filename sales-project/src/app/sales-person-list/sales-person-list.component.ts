c33bca6588db18d77a87594905224dc53abc8dfd
parmar-chirayu <cyparmar@my.yorku.ca>
2024-01-20 13:56:53 -0500

    Code Example

---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("John", "Doe", "john.doe@example.com", 50000)
+  ];
+
+  ngOnInit() {
+    this.salesPersonList.push(new SalesPerson("Jane", "Smith", "jane.smith@example.com", 60000));
+    this.salesPersonList.push(new SalesPerson("Alex", "Smith", "alex.smith@example.com", 70000));
+    this.salesPersonList.push(new SalesPerson("Mike", "Ross", "mike.ross@example.com", 80000));
+  }
+
+}
